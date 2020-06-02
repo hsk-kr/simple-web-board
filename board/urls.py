ed85c420334e17abb9f5c1a33a338ce8d165d75e
hsr-kr <hsk.coder@gmail.com>
2020-06-03 08:52:06 +0900

    board page

--- board/urls.py
+++ board/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import create_board, get_board, board_render
 
 urlpatterns = [
-    path("create_board", create_board),
-    path("get_board", get_board),
+    path("/create_board", create_board),
+    path("/get_board", get_board),
     path("", board_render),
 ]
--- board/views.py
+++ board/views.py
@@ -1,7 +1,9 @@
 from datetime import datetime, timedelta
 
 from django import forms
+from django.utils import timezone
 from django.http import HttpResponseBadRequest, JsonResponse, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 
 from .models import Board
@@ -12,20 +14,23 @@ class BoardForm(forms.Form):
     content = forms.CharField(max_length=2000)
 
 
+@csrf_exempt
 def create_board(request):
+    print(request.POST)
     f = BoardForm(request.POST)
     if not f.is_valid():
-        raise HttpResponseBadRequest()
+        return HttpResponseBadRequest()
     new_board = Board(title=request.POST["title"], content=request.POST["content"])
     new_board.save()
     return HttpResponse()
 
 
+@csrf_exempt
 def get_board(request):
-    a_week_ago = datetime.now() - timedelta(days=7)
-    Board.objects.filter(date__lte=a_week_ago).DELETE()
-    board = Board.objects.all()
-    return JsonResponse({data: board})
+    a_week_ago = timezone.localtime() - timedelta(days=7)
+    Board.objects.filter(date__lte=a_week_ago).delete()
+    board = Board.objects.order_by("-date")
+    return JsonResponse({"data": list(board.values())})
 
 
 def board_render(request):
